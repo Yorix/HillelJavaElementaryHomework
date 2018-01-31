785e5ba61de9fe31c438772ae7a726006573e085
Yorix <yorix82@gmail.com>
2018-01-31 10:30:18 +0200

    MyCalc

---
+++ src/MyCalc2/Start.java
@@ -0,0 +1,14 @@
+package MyCalc2;
+
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the expression using decimal numbers, +, -, *, or / ");
+        new Calc(in.nextLine());
+
+    }
+}
+
