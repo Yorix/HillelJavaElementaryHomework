84dd823da6fb2d53382ab005ff021b37b5684840
Yorix <yorix82@gmail.com>
2018-02-08 09:21:25 +0200

    task 3

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -3,14 +3,13 @@ package task3;
 import java.util.Scanner;
 
 public class Main {
-    private static int year;
-    private static int century;
-    private static int min = 1, max = 2018;
+    private static final int MIN = 1, MAX = 2018;
 
     public static void main(String[] args) {
-        year = getYear();
-        century = getCentury(year);
+        int year = getYear();
+        int century = getCentury(year);
         printCentury(year, century);
+        wantToContinue();
     }
 
     private static int getCentury(int year) {
@@ -19,7 +18,7 @@ public class Main {
 
     private static int getYear() {
         while (true) {
-            System.out.println("Enter the year from " + min + " to " + max);
+            System.out.println("Enter the year from " + MIN + " to " + MAX);
 
             String input = new Scanner(System.in).nextLine();
             StringBuilder year = new StringBuilder("0");
@@ -27,11 +26,11 @@ public class Main {
             for (int i = 0, len = input.length(); i < len; i++) {
                 if (input.charAt(i) >= '0' && input.charAt(i) <= '9') {
                     year.append(input.charAt(i));
-                } else if (Integer.parseInt(year.toString()) >= min && Integer.parseInt(year.toString()) <= max) {
+                } else if (Integer.parseInt(year.toString()) >= MIN && Integer.parseInt(year.toString()) <= MAX) {
                     return Integer.parseInt(year.toString());
                 }
             }
-            if (Integer.parseInt(year.toString()) < min || Integer.parseInt(year.toString()) > max) {
+            if (Integer.parseInt(year.toString()) < MIN || Integer.parseInt(year.toString()) > MAX) {
                 continue;
             }
             return Integer.parseInt(year.toString());
@@ -40,5 +39,13 @@ public class Main {
 
     private static void printCentury(int year, int century) {
         System.out.println(year + " is the " + century + " century.");
+    }
+
+    private static void wantToContinue() {
+        System.out.print("Continue? (y/n) ");
+        String input = new Scanner(System.in).next();
+        if (input.equals("y"))
+            main(null);
+        else System.exit(0);
     }
 }
